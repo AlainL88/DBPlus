c95242fcd18fb3b12b18a2fe49b639091da19ad3
Alain Lima <alainlima88@gmail.com>
2026-08-01 16:06:34 +0200

    Initial Commit

---
+++ DB+/DB_App.swift
@@ -0,0 +1,17 @@
+//
+//  DB_App.swift
+//  DB+
+//
+//  Created by Alain Lima on 01/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DB_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
